5db58bfdd99d847f3592d7ac65075cef7988c455
himelsu <himelsu@icloud.com>
2024-01-25 15:11:25 +0000

    Second Commit

---
+++ src/javascript/footer_year_auto_update.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+const copyrightElement = document.getElementById('copyright');
+copyrightElement.textContent = ` © ${currentYear} Lara Katerine. ALL RIGHTS RESERVED `;
